2f44171e4f84e703de5a4dc8d48a816ec58c27e1
EissaMD <eissa.code@gmail.com>
2023-06-12 14:27:43 +0300

    Add more entries (customer, tire and damege info)

--- Warranty_claim.py
+++ Warranty_claim.py
@@ -1,20 +1,17 @@
 import ttkbootstrap as ttk
-from tkinter.filedialog import askopenfilename
-from tkinter.scrolledtext import ScrolledText
-
 
 class App(ttk.Window):
     def __init__(self):
         super().__init__(
             title="إشعار ضمان",
             themename="flatly",
-            size=(700, 700),
+            size=(700, 800),
             # resizable=(False, False),
         )
         # title
         frame = ttk.Frame(self ,bootstyle="dark" )
         frame.pack(fill="both" , anchor="e")
-        ttk.Label(frame , text="إشعار ضمان" , font="arial 32 bold" ,  bootstyle="inverse" ,).pack()
+        ttk.Label(frame , text="إشعار الضمان" , font="arial 32 bold" ,  bootstyle="inverse" ,).pack()
         ttk.Separator(self,bootstyle="dark").pack(fill="both" , pady=4)
         # Basic information
         entries = (
@@ -26,7 +23,38 @@ class App(ttk.Window):
             ("Inspection Date:"     , "Inspection_date"     , "entry"   ,2,1 , None),
             ("Received On:"         , "received_on"         , "entry"   ,3,1 , None),
         )
-        self.basic_info = EntriesFrame(self,"Basic Information (المعلومات الاساسية)",entries)
+        self.basic_info = EntriesFrame(self,"Basic Info (المعلومات الاساسية)",entries)
+        # Customer and Vehicle information
+        entries = (
+            ("Customer Name:"   , "customer_name"   , "entry"   ,0,0 , None),
+            ("Customer Address:", "customer_address", "entry"   ,1,0 , None),
+            ("Contact Phone:"   , "contact_phone"   , "entry"   ,2,0 , None),
+            ("Car:"             , "car"             , "entry"   ,0,1 , None),
+            ("Door No:"         , "door_no"         , "entry"   ,1,1 , None),
+            ("Contact Email:"   , "contact_email"   , "entry"   ,2,1 , None),
+        )
+        self.customer_info = EntriesFrame(self,"Customer and Vehicle Info (معلومات العميل و المركبة)",entries)
+        # Tire information
+        entries = (
+            ("Tire:"                , "tire"            , "entry"   ,0,0 , None),
+            ("Tire category:"       , "tire_category"   , "entry"   ,1,0 , None),
+            ("Load Index [70-170]:" , "load_index"      , "entry" ,2,0 , (70,170)),
+            ("Origin:"              , "origin"          , "entry"   ,3,0 , None),
+            ("Tire Position:"       , "tire_position"   , "menu"    ,1,1 , ("FL","FR","BL","BR")),
+            ("Speed Rating:"        , "speed_rating"    , "entry"   ,2,1 , None),
+            ("DOT:"                 , "dot"             , "entry"   ,3,1 , None),
+        )
+        self.tire_info = EntriesFrame(self,"Tire Info (معلومات الإطار)",entries)
+        # Damage information
+        entries = (
+            ("Section:"         , "section"         , "entry"   ,0,0 , None),
+            ("Type:"            , "type"            , "entry"   ,1,0 , None),
+            ("Detailed Damage:" , "detailed_damage" , "entry"   ,2,0 , None),
+            ("PSI:"             , "psi"             , "entry"   ,0,1 , None),
+            ("BAR:"             , "bar"             , "entry"   ,1,1 , None),
+            ("KPA:"             , "kpa"             , "entry"   ,2,1 , None),
+        )
+        self.damage_info = EntriesFrame(self,"Damage Info (معلومات الضرر)",entries)
         
         
 class EntriesFrame(ttk.Labelframe):
@@ -37,19 +65,20 @@ class EntriesFrame(ttk.Labelframe):
         self.entries_frame = ttk.Frame(self); self.entries_frame.pack(fill="both",expand=True,padx=4,pady=4)
         for entry in entry_ls:
             self.add_entry(entry)
-        ttk.Button(self,command=self.get_data).pack()
+        # ttk.Button(self,command=self.get_data).pack()
     def add_entry(self,entry_info):
         label, entry_name , entry_type , row , col , options=entry_info
         self.entries_frame.grid_columnconfigure(col,weight=1)
         frame = ttk.Frame(self.entries_frame)
         frame.grid(sticky="we",row=row,column=col,padx=10)
         ttk.Label(frame , text=f"{label:<20}" , font="arial 10 bold",width=20).pack(side="left" ,anchor="w")
+        # entry type
         if entry_type == "entry":
             self.entry_dict[entry_name] = ttk.Entry(frame )
             self.entry_dict[entry_name].pack(side="left", fill="both" , expand=True)
         elif entry_type == "menu":
             self.entry_dict[entry_name] = ttk.StringVar()
-            option_menu = ttk.OptionMenu(frame ,self.entry_dict[entry_name] ,"Choose Option", *options)
+            option_menu = ttk.OptionMenu(frame ,self.entry_dict[entry_name] ,options[0], *options , bootstyle="outline")
             option_menu.pack(side="left", fill="both" , expand=True)
             option_menu.config(width=19)
             
