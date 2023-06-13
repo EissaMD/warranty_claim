50e3204b1874c03b53eb5f9f86d518b5355b3e23
EissaMD <eissa.code@gmail.com>
2023-06-13 15:49:19 +0300

    Create option to add product image

--- Warranty_claim.py
+++ Warranty_claim.py
@@ -1,11 +1,13 @@
 import ttkbootstrap as ttk
+from tkinter import filedialog
+import os , re
 
 class App(ttk.Window):
     def __init__(self):
         super().__init__(
             title="إشعار ضمان",
             themename="flatly",
-            size=(700, 800),
+            size=(700, 780),
             # resizable=(False, False),
         )
         # title
@@ -55,7 +57,25 @@ class App(ttk.Window):
             ("KPA:"             , "kpa"             , "entry"   ,2,1 , None),
         )
         self.damage_info = EntriesFrame(self,"Damage Info (معلومات الضرر)",entries)
-        
+        # add product images
+        frame = ttk.Labelframe(self,text="Add Images (إضافة صور)")
+        frame.pack(fill="x"  ,padx=2, pady=2)
+        headers = ("Image File", "Image Path")
+        self.img_table = InfoTable(frame,headers)
+        self.img_table.pack(fill="x" , expand=True ,side="left")
+        frame = ttk.Frame(frame); frame.pack(side="left",fill="y")
+        ttk.Button(frame,text="+" ,bootstyle="outline" , command=self.add_img).pack(fill="both",expand=True)
+        ttk.Button(frame,text="-" ,bootstyle="outline" , command=self.img_table.delete_selection).pack(fill="both",expand=True)
+    ###############        ###############        ###############        ###############        
+    def add_img(self,):
+        files = filedialog.askopenfilenames(filetypes=[('image files', ('.png', '.jpg' , '.jpeg' ))])
+        rows = []
+        for file in files:
+            row = (os.path.basename(file), file)
+            if row not in self.img_table.data:
+                rows.append(row)
+        self.img_table.add_rows(rows)
+##############################################################################################################
         
 class EntriesFrame(ttk.Labelframe):
     def __init__(self,master,title,entry_ls=()):
@@ -65,7 +85,7 @@ class EntriesFrame(ttk.Labelframe):
         self.entries_frame = ttk.Frame(self); self.entries_frame.pack(fill="both",expand=True,padx=4,pady=4)
         for entry in entry_ls:
             self.add_entry(entry)
-        # ttk.Button(self,command=self.get_data).pack()
+    ###############        ###############        ###############        ###############
     def add_entry(self,entry_info):
         label, entry_name , entry_type , row , col , options=entry_info
         self.entries_frame.grid_columnconfigure(col,weight=1)
@@ -87,14 +107,53 @@ class EntriesFrame(ttk.Labelframe):
         elif entry_type == "date":
             self.entry_dict[entry_name] = ttk.DateEntry(master=frame)
             self.entry_dict[entry_name].pack(side="left", fill="both" , expand=True)
-            
+    ###############        ###############        ###############        ###############
     def get_data(self):
         data = {}
         for entry_name in self.entry_dict:
             data[entry_name] = self.entry_dict[entry_name].get()
         print(data)
         return data
+##############################################################################################################
 
+class InfoTable(ttk.Treeview):
+    def __init__(self,master,headers=()):
+        self.data = [] # initialize empty tree
+        super().__init__(master, columns=headers, show="headings" , bootstyle="primary" , height=3,)
+        for header in headers:
+            label = header.replace("_", " ")
+            label = label.capitalize()
+            self.heading(header, text=label)
+    ###############        ###############        ###############        ###############
+    def clear(self):
+        self.data = []
+        self.delete(*self.get_children())
+    ###############        ###############        ###############        ###############
+    def add_rows(self,rows=None):
+        if rows is not None:
+            self.data.extend(rows)
+            for row in rows:
+                self.insert('', ttk.END, values=row)
+    ###############        ###############        ###############        ###############
+    def add_new_rows(self,rows=None):
+        if rows is not None:
+            self.data = rows
+            self.clear()
+            self.add_rows(rows)
+    ###############        ###############        ###############        ###############
+    def delete_selection(self):
+        for sel_item in self.selection():
+            self.delete(sel_item)
+            sel_item = int(re.sub("[^0-9]", "", sel_item))
+            self.data.pop(sel_item-1)
+##############################################################################################################
+
+class NewRow(ttk.Frame):
+    def __init__(self,master,column_percentage=(),padx=0,pady=0):
+        super().__init__(master)
+        self.pack(fill="both",padx=padx,pady=pady)
+        for column_no,column_wight in enumerate(column_percentage):
+            self.columnconfigure(column_no,weight=column_wight)
 if __name__ == '__main__':
     app = App()
     app.mainloop()
